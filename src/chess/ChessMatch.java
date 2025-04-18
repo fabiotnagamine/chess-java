d1b02f4582d9848ad76d57d69ed50eda2dda2dc2
fabiotnagamine <nagamine.fabio.1998@gmail.com]>
2025-04-18 19:47:43 -0300

    Adicionando turnos dos jogadores

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,9 +9,23 @@ import chess.pieces.Rook;
 public class ChessMatch {
 
 	private Board board;
+	private int turn;
+	private Color currentPlayer;
+	
+	public Color getCurrentPlayer() {
+		return currentPlayer;
+	}
+	
+	public int getTurn() {
+		return turn;
+	}
+
+
 
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
 	}
 
@@ -30,6 +44,7 @@ public class ChessMatch {
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 		return (ChessPiece)capturedPiece;
 		
 	}
@@ -43,15 +58,22 @@ public class ChessMatch {
 
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsPiece(position)){
-			throw new ChessException("Source not found");
-		} 
+			throw new ChessException("Error: Source not found");
+		}
+		if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()){
+			throw new ChessException("Error: Unable to make move, other player's turn");
+		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Error: There is no possible moves for the chosen piece");
+		}
 	}
 	
 	private void validateTargetPosition(Position source, Position target) {
 		if(!board.piece(source).possibleMove(target)) {
-			throw new ChessException("The chosen piece can't move to target position");
+			throw new ChessException("Error: The chosen piece can't move to target position");
 		}
 	}
+	
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
@@ -61,6 +83,10 @@ public class ChessMatch {
 		Position position = sourcePosition.toPosition();
 		validateSourcePosition(position);
 		return board.piece(position).possibleMoves();
+	}
+	
+	private void nextTurn() {
+		currentPlayer = (currentPlayer == Color.WHITE)? Color.BLACK : Color.WHITE;
 	}
 
 	private void initialSetup() {
