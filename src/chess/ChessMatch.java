78cd947f6027ccff48d2024760e8fb2fdd65a62d
fabiotnagamine <nagamine.fabio.1998@gmail.com]>
2025-04-29 13:35:48 -0300

    Pawn logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -74,7 +75,8 @@ public class ChessMatch {
 	}
 
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 
@@ -86,8 +88,9 @@ public class ChessMatch {
 	}
 
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece) board.removePiece(target);
 		board.placePiece(p, source);
+		p.decreaseMoveCount();
 		
 		if(capturedPiece != null) {
 			board.placePiece(capturedPiece, target);
@@ -191,14 +194,14 @@ public class ChessMatch {
 	private void initialSetup() {
 //		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
 //		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-//		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
 		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('d', 1, new King(board, Color.WHITE));
 
 //		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
 //		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-//		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
 		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('d', 8, new King(board, Color.BLACK));
